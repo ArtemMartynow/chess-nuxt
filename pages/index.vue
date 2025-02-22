3b6ab605dca2603aa41e09dfd55b95432bf02f46
ArtemMartynow <artem1435@i.ua>
2025-02-22 17:12:10 +0100

    added board

---
+++ pages/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="board">
+    <div
+      v-for="cell in board" 
+      :key="cell"
+      :class="{
+        'cell': true,
+        'white': cell.color === 'white',
+        'black': cell.color === 'black',
+        'purple': cell.color === 'purple'
+      }"
+      @click="move(cell)"
+    >
+    </div>
+  </div>  
+</template>
+
+<script setup>
+let board = reactive([])
+let startCell = ref(null)
+let cellColor = ref('')
+let activeMoves = ref([])
+
+for (let i = 0; i < 8; i++) {
+  for (let j = 0; j < 8; j++) {
+    const cell = {
+      x: j,
+      y: i,
+      color: (i + j) % 2 === 0 ? 'black' : 'white',
+      piece: ''
+    };
+    board.push(cell);
+  }
+}
+</script>
