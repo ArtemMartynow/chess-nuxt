989f268d11972863f6158824032eb14d2efc3938
ArtemMartynow <artem1435@i.ua>
2025-02-22 16:57:48 +0100

    plugged in dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: [
+    '@nuxtjs/tailwindcss'
+  ],
+  css: ['~/assets/scss/main.scss'],
+  app: { 
+    head: { 
+      title: 'Chess', 
+      meta: [ { name: 'description', content: 'Chess' } ] 
+    } 
+  },
+  ssr: true,
 })
