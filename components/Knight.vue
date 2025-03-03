31dac76f94afaf85d091f185139fa872de4453ee
ArtemMartynow <artem1435@i.ua>
2025-03-03 21:35:59 +0100

    added movement to the knight

--- components/Knight.vue
+++ components/Knight.vue
@@ -5,8 +5,56 @@
 </template>
 
 <script setup>
+const mainStore = useMainStore()
+
 const props = defineProps({
   board: Array,
   piece: String
 })
+
+watch(
+  () => mainStore.startCell,
+  (newStartCell) => {
+    if (
+        newStartCell && 
+        (mainStore.selectedFigure === 'knight_black' ||
+        mainStore.selectedFigure === 'knight_white')
+      ) {
+        movesKnight()
+    }
+  }
+)
+
+let movesKnight = () => {
+  let directions = [
+    { dx: 1, dy: 2 },
+    { dx: 1, dy: -2 },
+    { dx: -1, dy: 2 },
+    { dx: -1, dy: -2 },
+    { dx: 2, dy: 1 },
+    { dx: 2, dy: -1 },
+    { dx: -2, dy: 1 },
+    { dx: -2, dy: -1 }
+  ]
+
+  directions.forEach(({ dx, dy }) => {
+    let direction = 1
+    for (let step = 1; step < 8; step++) {
+      let newX = mainStore.startCell.x + dx * direction
+      let newY = mainStore.startCell.y + dy * direction
+      
+      let targetCell = props.board.find(i => i.x === newX && i.y === newY)
+      if (!targetCell) break
+
+      if (targetCell.piece === '') {
+        mainStore.activeMoves.push(targetCell)
+      } else {
+        if (targetCell.piece.slice(-5) !== mainStore.startCell.piece.slice(-5)) {
+          mainStore.activeMoves.push(targetCell)
+        }
+        break
+      }
+    }
+  })
+}
 </script>
